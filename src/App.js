f4430a748e915b2ac97ed09d6c6582a9b14f435c
phimanhhai01 <19020277@vnu.edu.vn>
2021-04-13 22:33:40 +0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+function App() {
+  //State stuff
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  // USE EFFECT
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+  
+  
+  // Functions
+  const filterHandler = () => {
+    switch(status){
+      case "completed":
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>Hai's Todo List</h1>
+      </header>
+      <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList filteredTodos = {filteredTodos} setTodos={setTodos} todos={todos} inputText={inputText}/>
+    </div>
+  );
+};
+
+export default App;
